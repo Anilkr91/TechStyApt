ad1345c6c0d519ef4ceb3555f68fbd5d0a567167
admin <admin@Viveks-MacBook-Pro.local>
2017-08-23 15:54:02 +0530

    resolved blue folder problem

--- StayAPT/Views/MenuTableViewCell.swift
+++ StayAPT/Views/MenuTableViewCell.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2017 Techximum. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class MenuTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var saImageView: UIImageView!
+    @IBOutlet weak var saTitleLabel: UILabel!
+    @IBOutlet weak var saSubTitleLabe: UILabel!
+
+}
+
