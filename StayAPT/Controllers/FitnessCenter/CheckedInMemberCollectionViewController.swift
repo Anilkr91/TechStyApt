3f50f3071482d93275144543ab1697656cb4f23c
admin <admin@Viveks-MacBook-Pro.local>
2017-08-27 13:39:49 +0530

    Resolved the checkedin member collection view cell spacing issue

--- StayAPT/Controllers/FitnessCenter/CheckedInMemberCollectionViewController.swift
+++ StayAPT/Controllers/FitnessCenter/CheckedInMemberCollectionViewController.swift
@@ -33,20 +33,20 @@ class CheckedInMemberCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return memberArray.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CheckedInMemberCollectionViewCell
         
@@ -55,5 +55,43 @@ class CheckedInMemberCollectionViewController: UICollectionViewController {
         cell.saCountLabel.text = memberArray[indexPath.item].counter
         return cell
     }
-
 }
+
+/*extension CheckedInMemberCollectionViewController: UICollectionViewDelegateFlowLayout {
+    fileprivate var sectionInsets: UIEdgeInsets {
+        return .init(top: 8, left: 8, bottom: 8, right: 8)
+        
+    }
+    
+    fileprivate var itemsPerRow: CGFloat {
+        return 3
+        
+    }
+    
+    fileprivate var interitemSpace: CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sectionPadding = sectionInsets.left * (itemsPerRow + 1)
+        let interitemPadding = max(0.0, itemsPerRow - 1) * interitemSpace
+        let availableWidth = collectionView.bounds.width - sectionPadding - interitemPadding
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem+30)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5.0
+    }
+}*/
