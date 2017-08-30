a4f7f67faf689f727fc9211a8d0218b30fb4ec32
admin <admin@Viveks-MacBook-Pro.local>
2017-08-31 00:31:48 +0530

    date picker added in PersonalDetailTableViewController

--- StayAPT/Controllers/FitnessCenter/PersonalDetailTableViewController.swift
+++ StayAPT/Controllers/FitnessCenter/PersonalDetailTableViewController.swift
@@ -9,9 +9,24 @@
 import UIKit
 
 class PersonalDetailTableViewController: BaseTableViewController {
+    
+    //Properties
+    @IBOutlet weak var aboutMeTextField: BaseTextField!
+    @IBOutlet weak var firstNameTextField: BaseTextField!
+    @IBOutlet weak var lastNameTextField: BaseTextField!
+    @IBOutlet weak var genderSegmentationControl: UISegmentedControl!
+    @IBOutlet weak var dateOfBirthTextField: BaseTextField!
+    @IBOutlet weak var heightTextField: BaseTextField!
+    @IBOutlet weak var weightTextField: BaseTextField!
+    
+    
+    lazy var dateOfBirthDatePicker = UIDatePicker()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dateOfBirthDatePicker.datePickerMode = .date
+        dateOfBirthTextField.inputView = dateOfBirthDatePicker
 
     }
 }
