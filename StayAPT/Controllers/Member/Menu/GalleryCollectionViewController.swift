f478c55efdd2aa8c914aaff07390cb7f91513d5b
admin <admin@Viveks-MacBook-Pro.local>
2017-08-31 10:50:07 +0530

    fixed gallery design

--- StayAPT/Controllers/Member/Menu/GalleryCollectionViewController.swift
+++ StayAPT/Controllers/Member/Menu/GalleryCollectionViewController.swift
@@ -32,66 +32,66 @@ class GalleryCollectionViewController: BaseCollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
         return memberArray.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! GalleryCollectionViewCell
-    
+        
         cell.member = memberArray[indexPath.item]
         return cell
     }
 }
 
 extension GalleryCollectionViewController: UICollectionViewDelegateFlowLayout {
- fileprivate var sectionInsets: UIEdgeInsets {
- return .init(top: 8, left: 8, bottom: 8, right: 8)
- 
- }
- 
- fileprivate var itemsPerRow: CGFloat {
- return 2
- 
- }
- 
- fileprivate var interitemSpace: CGFloat {
- return 0.0
- }
- 
- func collectionView(_ collectionView: UICollectionView,
- layout collectionViewLayout: UICollectionViewLayout,
- sizeForItemAt indexPath: IndexPath) -> CGSize {
- let sectionPadding = sectionInsets.left * (itemsPerRow + 1)
- let interitemPadding = max(0.0, itemsPerRow - 1) * interitemSpace
- let availableWidth = collectionView.bounds.width - sectionPadding - interitemPadding
- let widthPerItem = availableWidth / itemsPerRow
- 
- return CGSize(width: widthPerItem, height: widthPerItem)
- }
- 
- func collectionView(_ collectionView: UICollectionView,
- layout collectionViewLayout: UICollectionViewLayout,
- insetForSectionAt section: Int) -> UIEdgeInsets {
- return sectionInsets
- }
- 
- func collectionView(_ collectionView: UICollectionView,
- layout collectionViewLayout: UICollectionViewLayout,
- minimumLineSpacingForSectionAt section: Int) -> CGFloat {
- return 5.0
- }
- }
+    fileprivate var sectionInsets: UIEdgeInsets {
+        return .init(top: 8 , left: 8, bottom: 8, right: 8)
+        
+    }
+    
+    fileprivate var itemsPerRow: CGFloat {
+        return 2
+        
+    }
+    
+    fileprivate var interitemSpace: CGFloat {
+        return 5.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let sectionPadding = sectionInsets.left * (itemsPerRow + 1)
+        let interitemPadding = max(0.0, itemsPerRow - 1) * interitemSpace
+        let availableWidth = collectionView.bounds.width - sectionPadding - interitemPadding
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8.0
+    }
+}
