8a6773a26aadc85c647a7aa489743d130e43337c
admin <admin@Viveks-MacBook-Pro.local>
2017-09-12 18:41:32 +0530

    register and login services added

---
+++ StayAPT/Services/LoginService+POST.swift
@@ -0,0 +1,43 @@
+//
+//  LoginService+POST.swift
+//  StayAPT
+//
+//  Created by admin on 12/09/17.
+//  Copyright © 2017 Techximum. All rights reserved.
+//
+
+import Alamofire
+import Gloss
+
+class LoginPostService {
+    static func executeRequest (completionHandler: @escaping ([ClassModel]) -> Void) {
+        
+        Loader.sharedInstance.showLoader()
+        let header: HTTPHeaders = ["X_API_KEY" : Constants.API_KEY]
+        let URL = Constants.BASE_URL
+        
+        let manager = Alamofire.SessionManager.default
+        manager.session.configuration.timeoutIntervalForRequest = 60
+        
+        let request = manager.request( URL + "login", method: .post, parameters: nil, encoding: JSONEncoding.default, headers: header).responseJSON { response in
+            
+            switch response.result {
+            case .success(let value) :
+                if let data = ClassModelArray.init(json: value as! JSON)  {
+                    completionHandler(data.results)
+                    Loader.sharedInstance.hideLoader()
+                    
+                } else {
+                    Loader.sharedInstance.hideLoader()
+                    let error = ErrorModel.init(json: value as! JSON)
+                    Alert.showAlertWithMessage(title: "Error", message: error!.message)
+                }
+                
+            case .failure(let error):
+                Loader.sharedInstance.hideLoader()
+                Alert.showAlertWithMessage(title: "Error", message: error.localizedDescription)
+            }
+        }
+        debugPrint(request)
+    }
+}
