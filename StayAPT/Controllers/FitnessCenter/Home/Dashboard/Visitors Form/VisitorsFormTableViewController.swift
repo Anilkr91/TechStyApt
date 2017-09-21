f1a52a108289a4f242cca953a3679bf9337fd3c5
admin <admin@Viveks-MacBook-Pro.local>
2017-09-21 15:48:45 +0530

    validation added in visitors form

--- StayAPT/Controllers/FitnessCenter/Home/Dashboard/Visitors Form/VisitorsFormTableViewController.swift
+++ StayAPT/Controllers/FitnessCenter/Home/Dashboard/Visitors Form/VisitorsFormTableViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class VisitorsFormTableViewController: BaseTableViewController {
-
+    
+    // Properties
+    @IBOutlet weak var fullNameTextField: BaseTextField!
+    @IBOutlet weak var dateOfBirthTextField: BaseTextField!
+    @IBOutlet weak var dateTextField: BaseTextField!
+    @IBOutlet weak var mobileNumberTextField: BaseTextField!
+    @IBOutlet weak var emailTextField: BaseTextField!
+    @IBOutlet weak var remainderTextField: BaseTextField!
+    @IBOutlet weak var visitorsTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,4 +29,34 @@ class VisitorsFormTableViewController: BaseTableViewController {
         cell.selectionStyle = .none
     }
 
+    @IBAction func SubmitButtonTapped(_ sender: Any) {
+    
+        let fullName = fullNameTextField.text!
+        let dateOfBirth = dateOfBirthTextField.text!
+        let date = dateTextField.text!
+        let mobileNumber = mobileNumberTextField.text!
+        let email = emailTextField.text!
+        let remainder = remainderTextField.text!
+        
+        if fullName.removeAllSpaces().isEmpty {
+            print("fullName all validation")
+            
+        } else if dateOfBirth.removeAllSpaces().isEmpty {
+            print("dateOfBirth all validation")
+            
+        } else if date.removeAllSpaces().isEmpty {
+            print("date all validation")
+        } else if mobileNumber.removeAllSpaces().isEmpty {
+            print("mobileNumber all validation")
+            
+        } else if email.removeAllSpaces().isEmpty {
+            print("registrationFee all validation")
+            
+        } else if remainder.removeAllSpaces().isEmpty {
+            print("totalPaid all validation")
+            
+        }  else {
+            print("passed all validation")
+        }
+    }
 }
